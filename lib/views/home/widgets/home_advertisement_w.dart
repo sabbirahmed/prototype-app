b035b1196c8cef99efe21f43e7bffb5d4a4d28d6
khondakar.afridi <afridi.khondakar@gmail.com>
2024-01-02 08:52:12 +0600

    Code refactoring

--- lib/views/home/widgets/home_advertisement_w.dart
+++ lib/views/home/widgets/home_advertisement_w.dart
@@ -19,41 +19,37 @@ class HomeAdvertisement extends StatelessWidget {
             const SizedBox(
               height: 250,
               child: CommonNetworkImageWidget(
-                imageLink: "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+                imageLink:
+                    "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
                 boxFit: BoxFit.cover,
               ),
-              // CommonNetworkImageWidget(imageLink: item),
             ),
             Gap(10.h),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-
-
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const Text("Price Down",
+                    const Text(
+                      "Price Down",
                       style: titleMedium20,
                     ),
-
-                    Text("Dream Bag for a Dream\nPrice",
+                    Text(
+                      "Dream Bag for a Dream\nPrice",
                       style: bodyRegular16.copyWith(color: AppColors.grey),
                     ),
                   ],
                 ),
-
                 Container(
                   color: AppColors.black,
                   padding: EdgeInsets.symmetric(vertical: 10.h, horizontal: 30.w),
                   child: Text(
                     "SHOP NOW",
                     style: bodyMedium16.copyWith(color: Colors.amberAccent),
-
                   ),
                 )
-
               ],
             )
           ],
--- lib/views/home/widgets/home_carousel_slider_w.dart
+++ lib/views/home/widgets/home_carousel_slider_w.dart
@@ -13,7 +13,6 @@ class HomeCarouselSlider extends StatefulWidget {
 }
 
 class _HomeCarouselSliderState extends State<HomeCarouselSlider> {
-
   int currentIndex = 0;
   final CarouselController _controller = CarouselController();
 
@@ -34,16 +33,18 @@ class _HomeCarouselSliderState extends State<HomeCarouselSlider> {
               });
             },
           ),
-          items: DummyProductListModelHandler().dummyProductList.reversed
+          items: DummyProductListModelHandler()
+              .dummyProductList
+              .reversed
               .map(
                 (item) => ClipRRect(
-              borderRadius: BorderRadius.circular(0),
-              child: CommonNetworkImageWidget(
-                imageLink: item.imageLink.toString(),
-                boxFit: BoxFit.cover,
-              ),
-            ),
-          )
+                  borderRadius: BorderRadius.circular(0),
+                  child: CommonNetworkImageWidget(
+                    imageLink: item.imageLink.toString(),
+                    boxFit: BoxFit.cover,
+                  ),
+                ),
+              )
               .toList(),
         ),
         Positioned(
@@ -52,25 +53,22 @@ class _HomeCarouselSliderState extends State<HomeCarouselSlider> {
           left: 0,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: DummyProductListModelHandler().dummyProductList.asMap().entries.map((entry) {
-              return GestureDetector(
-                onTap: () => _controller.animateToPage(entry.key),
-                child:
-                AnimatedContainer(
-                  duration: const Duration(milliseconds: 300),
-                  height: 8.h,
-                  width: (currentIndex == entry.key) ? 30.w : 10.w,
-                  margin: EdgeInsets.symmetric(horizontal: 5.w),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      color: (currentIndex == entry.key) ? Colors.black : Colors.grey
+            children: DummyProductListModelHandler().dummyProductList.asMap().entries.map(
+              (entry) {
+                return GestureDetector(
+                  onTap: () => _controller.animateToPage(entry.key),
+                  child: AnimatedContainer(
+                    duration: const Duration(milliseconds: 300),
+                    height: 8.h,
+                    width: (currentIndex == entry.key) ? 30.w : 10.w,
+                    margin: EdgeInsets.symmetric(horizontal: 5.w),
+                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(5), color: (currentIndex == entry.key) ? Colors.black : Colors.grey),
                   ),
-                ),
-              );
-            }).toList(),
+                );
+              },
+            ).toList(),
           ),
         ),
-
       ],
     );
   }
